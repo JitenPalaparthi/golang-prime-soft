09969496d422a917b2a3e5810686e9efc7fda1dc
Jiten <jpalaparthi@gmail.com>
2024-12-25 11:11:07 +0530

    add more examples and tasks on slices

---
+++ 15-slice/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	slice1 := []int{10, 11, 12, 13, 14, 15, 16, 17, 18, 19}
+	slice2 := slice1[:] // shallow copy
+
+	slice3 := slice1[:5]
+	slice4 := slice1[2:5]
+	slice5 := slice1[5:]
+
+	PrintSliceHeader(slice1, "slice1")
+	PrintSliceHeader(slice2, "slice2")
+	PrintSliceHeader(slice3, "slice3")
+	PrintSliceHeader(slice4, "slice4")
+	PrintSliceHeader(slice5, "slice5")
+
+	slice6 := append(slice1[:2], slice1[3:]...)
+	fmt.Println(slice6)
+
+}
+
+func PrintSliceHeader(slice []int, name string) {
+	fmt.Println("----------------------------")
+	fmt.Println(name)
+	fmt.Println("Elements->", slice)
+	fmt.Printf("Address of the slice Header->%p\n", &slice)
+	if len(slice) > 0 {
+		fmt.Printf("Pointer of the slice->%p\n", &slice[0])
+	}
+	fmt.Printf("len:%d cap: %d \n ", len(slice), cap(slice))
+	//fmt.Println("----------------------------")
+}
+
+func deleteElem(slice []int, index int) ([]int, error) {
+
+	return nil, nil
+}
---
+++ tasks/6-task-slice-delete.md
@@ -0,0 +1,5 @@
+take example 15-slice
+
+Write a function to delete an element from the slice and return 
+
+func deleteElem(slice []int,index int)([]slice,error)
